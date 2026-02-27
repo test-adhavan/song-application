9f3051f97996e99b3c383034c92c0b2645126fc3
test <test.adhavan@example.com>
2026-02-27 11:16:20 +0530

    setup finished

---
+++ src/test/java/com/task/song/SongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
